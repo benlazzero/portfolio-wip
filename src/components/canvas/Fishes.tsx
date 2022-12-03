9dd238ae4ad9440578e251c3c5128e94dc90a305
benlazzero <85390423+benlazzero@users.noreply.github.com>
2022-12-02 23:04:22 -0800

    fishes tweaking

--- src/components/canvas/Fishes.tsx
+++ src/components/canvas/Fishes.tsx
@@ -1,59 +1,59 @@
 import * as THREE from 'three'
 import { useFrame, useThree } from '@react-three/fiber'
-import { useRef, useState, Suspense, Fragment } from 'react'
-import { shaderMaterial, useGLTF } from "@react-three/drei";
+import { useRef, useState, Suspense, Fragment, useEffect, forwardRef, Children } from 'react'
+import { shaderMaterial, useGLTF, Float } from "@react-three/drei";
 import { useSpring, animated } from '@react-spring/three'
 
 function Fish({...props}) {
-  const mesh = useRef()
+  const ref = useRef()
   const { nodes, materials } = useGLTF('/texture/fish-transformed.glb')
   const [active, setActive] = useState(false)
-  const springs = useSpring({ color: active ? 'red' : 'blue' })
+  const { viewport, camera } = useThree()
   
-  const spring = useSpring({
-    color: active ? "hotpink" : "blue"
-  });
-
   return (
-    <animated.mesh 
-      ref={mesh}
-      geometry={nodes.fish.geometry} 
-      {...props} 
-      scale={0.04}
-      material-color = {spring.color}
-      onClick={() => setActive(!active)}
-      >
-    </animated.mesh>
+    <Float floatIntensity={5} floatingRange={[1, 3]} speed={4}>
+      <animated.mesh 
+        ref={ref}
+        geometry={nodes.fish.geometry} 
+        {...props} 
+        scale={0.04}
+        material-color = {"yellow"}
+        >
+      </animated.mesh>
+    </Float>
   )
 }
 
 function FishGroup(props) {
   const ref = useRef()
-  const { viewport } = useThree()
+  let { z } = props
+  const { viewport, camera } = useThree()
+  let { width, height } = viewport.getCurrentViewport(camera, [0, 0, z])
   
   useFrame(() => {
-    ref.current.position.x += 0.05 
-    if (ref.current.position.x > viewport.width / 1.3) {
-      ref.current.position.x = -viewport.width / 1.3
+    ref.current.position.x += 0.02 
+    if (ref.current.position.x > width / 1.5) {
+      ref.current.position.x = -width / 1.5
     }
   })
   
   return (
-    <group ref={ref} scale={0.04} {...props}>
-      <Fish position={[0, 0, 0]} />
+    <group ref={ref} scale={0.04} {...props} innerRef={ref}>
+      <Fish position={[0, 0, -15]} />
       <Fish position={[19, 10, 0]} />
-      <Fish position={[-19, -10, 0]} />
+      <Fish position={[-19, -10, -2]} />
       <Fish position={[-17, 8, 0]} />
+      <Fish position={[-11, 8, -3]} />
     </group>
   )
 }
 
-  
-
 export default function Fishes() {
+  const z = 0
+  
   return (
     <Suspense fallback={null}>
-      <FishGroup position={[-1, 0, 0]} />
+      <FishGroup position={[-1, 0, z]} z={z} />
     </Suspense>
   )
 }
